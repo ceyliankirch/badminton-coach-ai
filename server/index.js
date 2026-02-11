e4a66acb314bd9c896f3fb01daba2dda14691cde
Ceylian Kirchhoffer <ceyliankirch@gmail.com>
2026-02-12 00:00:52 +0100

    yarn start added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Serveur pro sur le port ${PORT}`));
 
 // ==========================================
 // CONFIGURATION
