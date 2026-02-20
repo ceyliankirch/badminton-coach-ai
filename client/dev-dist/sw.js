61ebcd8d42f3ae1b1db5f1dfc662222961582c19
Ceylian Kirchhoffer <ceyliankirch@gmail.com>
2026-02-20 15:52:31 +0100

    logo adjustment

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-46f6dd99'], (function (workbox) { 'use strict';
+define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-46f6dd99'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r1k8ga2vvr"
+    "revision": "0.3s9vesetvfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
