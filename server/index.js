87f49eb0c87f9e70f9549c90552214d21c935932
Ceylian Kirchhoffer <ceyliankirch@gmail.com>
2026-02-28 14:53:18 +0100

    domain updated

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "https://badminton-coach-client.onrender.com", 
   "https://badminton-coach-ai.onrender.com",
-  "https://www.badmin.ceyliankirch.fr"
+  "https://badmin.ceyliankirch.fr" // 👈 AJOUTE TON NOUVEAU SOUS-DOMAINE ICI
 ];
 
 app.use(cors({
